9114bdd1223ad9bb31c92cdae81b69abd5be902e
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-09-01 19:22:55 +0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,25 +1,39 @@
 
 public class Student {
+    
     private String name;
+    
     private int age;
+    
     private int rollno;
     
-    private void setdata(String n, int a,int r)
-    {
+ private void setdata(String n, int a,int r){
+        
         name=n;
         age=a;
         rollno=r;
+        
     }
-    private void print(){
+    
+ private void print(){
+        
         System.out.println(name);
+        
         System.out.println(age);
+        
         System.out.println(rollno);
+        
     }
     
 
 public static void main(String[]args){
+    
 Student s1=new Student();
+    
 s1.setdata("waleed",22,135);
+    
 s1.print();
+    
 }
+    
 }
